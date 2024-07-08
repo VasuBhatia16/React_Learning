c64f2a6947aa25396a5313837ace7feade3d177c
Vasu Bhatia <vasubhatia@dldavppcbsei.com>
2024-07-08 20:15:50 +0530

    Updated App.jsx

--- 06currencyConvertor/src/App.jsx
+++ 06currencyConvertor/src/App.jsx
@@ -36,7 +36,7 @@ function App() {
                 <form
                     onSubmit={(e) => {
                         e.preventDefault();
-                      convert()
+                        convert()
                     
                     }}
                 >
